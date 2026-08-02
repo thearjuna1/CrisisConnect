29e042992537eabb557d6c947db0a6cd3861d690
thearjuna1 <thearjun077@gmail.com>
2026-08-02 13:38:27 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/crisisconnect/CrisisConnect/CrisisConnectApplication.java
@@ -0,0 +1,13 @@
+package com.crisisconnect.CrisisConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrisisConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrisisConnectApplication.class, args);
+	}
+
+}
